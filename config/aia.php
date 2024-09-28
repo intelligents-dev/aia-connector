5c1281a7f2d7afbd65589ae4feae059616f416cd
Jeffrey van Hees <jeffvanhees@gmail.com>
2024-09-28 14:58:46 +0200

    Fixes

--- config/aia.php
+++ config/aia.php
@@ -4,5 +4,7 @@ return [
     'base_url' => env('AIA_URL'),
     'token' => env('AIA_TOKEN'),
     'image_callback_url' => env('AIA_IMAGE_CALLBACK_URL'),
+    'face_image_callback_url' => env('AIA_FACE_IMAGE_CALLBACK_URL'),
+    'profile_image_callback_url' => env('AIA_PROFILE_IMAGE_CALLBACK_URL'),
     'ignore_ssl' => env('AIA_IGNORE_SSL', false),
 ];
